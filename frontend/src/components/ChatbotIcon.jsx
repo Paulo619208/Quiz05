c0c9f8965a1e8a54e08829df6005f37df9075616
Pauleen Tabor <pitabor@student.hau.edu.ph>
2026-03-10 19:55:39 +0800

    feat: add ChatbotIcon and UserIcon components

---
+++ frontend/src/components/ChatbotIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatbotIcon() {
+return (
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24px" height="24px">
+<path d="M12 2C6.477 2 2 6.477 2 12c0 1.657.507 3.197 1.379 4.5L2 22l5.5-1.379A9.953 9.953 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2zm-3.5 8a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm7 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z" />
+</svg>
+)
+}
+
+export default ChatbotIcon
