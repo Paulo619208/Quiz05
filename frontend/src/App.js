e79996d6376a5c05c3d2ac34fef08dfdb1d2071d
Pauleen Tabor <pitabor@student.hau.edu.ph>
2026-03-10 19:20:42 +0800

    feat: implement routing with LoginScreen and HomeScreen components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <LoginScreen  />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginScreen />} />
+        <Route path='/home' element={<HomeScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
