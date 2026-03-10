4adb61a588a3421aece02aeb3f68d90cb8c88c4a
Pauleen Tabor <pitabor@student.hau.edu.ph>
2026-03-10 18:36:51 +0800

    feat: update frontend dependencies and add login screen
    - Added @redux-devtools/extension, @reduxjs/toolkit, axios, react-bootstrap, react-redux, react-router-bootstrap, react-router-dom, redux, and redux-thunk to package.json and package-lock.json.
    - Included Font Awesome stylesheet in index.html for icon usage.
    - Removed App.css and associated styles from App.js, replacing it with a new LoginScreen component.
    - Created FormContainer component for consistent form styling.
    - Implemented LoginScreen component with email and password fields.
    - Deleted unused test and setup files for cleaner project structure.
    - Added requirements.txt for backend dependencies.

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import FormContainer from '../components/FormContainer'
+import { Form } from 'react-bootstrap'
+
+function LoginScreen() {
+  return (
+    <FormContainer>
+        <div className='text-center mb-4'>
+            <h1>Welcome Back!</h1>
+            <p>Please Sign In to your account</p>
+        </div>
+
+        <Form>
+            <Form.Group controlId='email' className='mb-3'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type='email'
+                    placeholder='Enter email'
+                ></Form.Control>
+            </Form.Group>
+           
+            <Form.Group controlId='password' className='mb-3'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Enter password'
+                ></Form.Control>
+            </Form.Group>
+
+            <button type='submit' className='btn btn-primary w-100'>Sign In</button>
+
+        </Form>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
